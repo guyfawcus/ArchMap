1ce8d7b8bdf9e74f0a3335b83ed0950c3823920c
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-02-08 22:26:31 +0000

    Stripped whitespace and reset prompt number to 1

--- wikifork-convert.py
+++ wikifork-convert.py
@@ -12,16 +12,16 @@ geo_output = ('')
 
 for line in wikitext:
     split = line.split('"')
-    
+
     coord = split[0].strip(' ')
     coord = coord.split(',')
     name = split[1].strip()
-    
+
     point = Point((float(coord[0]), float(coord[1])))
     feature = Feature(geometry=point, properties={"Name": name})
-    
+
     geo_output += str(feature)
-    
+
 output.write(geo_output)
 
 wikitext.close()
