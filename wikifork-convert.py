bd8c3ab34cfd4ff5a08be179da9650b00e9b02b9
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-02-08 22:15:02 +0000

    Fixed notebook script and added the raw python

---
+++ wikifork-convert.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+from geojson import Feature, Point
+
+wikitext = open('wiki-fork', 'r')
+output = open('output.geojson', 'w')
+
+geo_output = ('')
+
+for line in wikitext:
+    split = line.split('"')
+    
+    coord = split[0].strip(' ')
+    coord = coord.split(',')
+    name = split[1].strip()
+    
+    point = Point((float(coord[0]), float(coord[1])))
+    feature = Feature(geometry=point, properties={"Name": name})
+    
+    geo_output += str(feature)
+    
+output.write(geo_output)
+
+wikitext.close()
+output.close()
+
+# <codecell>
+
+
