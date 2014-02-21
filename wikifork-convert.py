ad8eb9b386e59bf82730f954f28ae7c2a714c6ef
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-02-21 01:59:26 +0000

    Changed to sane variable names and added 'comment'

--- wikifork-convert.py
+++ wikifork-convert.py
@@ -7,27 +7,31 @@ wiki = urlopen("https://wiki.archlinux.org/index.php/ArchMap/List")
 
 wiki_source = wiki.read().decode()
 
-wikitext_start = wiki_source.find('<pre>', wiki_source.find('<pre>') + 1) + 6
-wikitext_end = wiki_source.find('</pre>', wiki_source.find('</pre>') + 1) - 1
-wikitext = wiki_source[wikitext_start:wikitext_end]
+wiki_text_start = wiki_source.find('<pre>', wiki_source.find('<pre>') + 1) + 6
+wiki_text_end = wiki_source.find('</pre>', wiki_source.find('</pre>') + 1) - 1
+wiki_text = wiki_source[wiki_text_start:wiki_text_end]
 
-wiki_output = open('wiki-fork', 'w')
-wiki_output.write(wikitext)
+wiki_output = open('users.txt', 'w')
+wiki_output.write(wiki_text)
 wiki_output.close()
 
-wikitext = open('wiki-fork', 'r')
+users = open('users.txt', 'r')
 output = open('output.geojson', 'w')
 
 geo_output = []
 
-for line in wikitext:
-    split = line.split('"')
+for line in users:
+    elements = line.split('"')
 
-    coord = split[0].strip(' ')
-    coord = coord.split(',')
-    name = split[1].strip()
+    coords = elements[0].strip(' ')
+    coords = coords.split(',')
+    latitude = float(coords[0])
+    longitude = float(coords[1])
+    name = elements[1].strip()
+    comment = elements[2].strip()
+    comment = comment[2:]
 
-    point = Point((float(coord[1]), float(coord[0])))
+    point = Point((longitude, latitude))
     feature = Feature(geometry=point, properties={"Name": name})
 
     geo_output.append(feature)
@@ -35,5 +39,5 @@ for line in wikitext:
 
 output.write(dumps(FeatureCollection(geo_output)))
 
-wikitext.close()
+users.close()
 output.close()
