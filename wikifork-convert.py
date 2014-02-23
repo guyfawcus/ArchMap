42e452c0c1fda4aa7ce3c3b353bfb39c7565cbf0
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-02-23 11:37:12 +0000

    Moved code into functions

--- wikifork-convert.py
+++ wikifork-convert.py
@@ -3,41 +3,44 @@
 from urllib.request import urlopen
 from geojson import Feature, Point, FeatureCollection, dumps
 
-wiki = urlopen("https://wiki.archlinux.org/index.php/ArchMap/List")
 
-wiki_source = wiki.read().decode()
+def get_users():
+    wiki = urlopen("https://wiki.archlinux.org/index.php/ArchMap/List")
 
-wiki_text_start = wiki_source.find('<pre>', wiki_source.find('<pre>') + 1) + 6
-wiki_text_end = wiki_source.find('</pre>', wiki_source.find('</pre>') + 1) - 1
-wiki_text = wiki_source[wiki_text_start:wiki_text_end]
+    wiki_source = wiki.read().decode()
 
-wiki_output = open('users.txt', 'w')
-wiki_output.write(wiki_text)
-wiki_output.close()
+    wiki_text_start = wiki_source.find('<pre>', wiki_source.find('<pre>') + 1) + 6
+    wiki_text_end = wiki_source.find('</pre>', wiki_source.find('</pre>') + 1) - 1
+    wiki_text = wiki_source[wiki_text_start:wiki_text_end]
 
-users = open('users.txt', 'r')
-output = open('output.geojson', 'w')
-
-geo_output = []
-
-for line in users:
-    elements = line.split('"')
-
-    coords = elements[0].strip(' ')
-    coords = coords.split(',')
-    latitude = float(coords[0])
-    longitude = float(coords[1])
-    name = elements[1].strip()
-    comment = elements[2].strip()
-    comment = comment[2:]
-
-    point = Point((longitude, latitude))
-    feature = Feature(geometry=point, properties={"Name": name})
-
-    geo_output.append(feature)
+    wiki_output = open('users.txt', 'w')
+    wiki_output.write(wiki_text)
+    wiki_output.close()
 
 
-output.write(dumps(FeatureCollection(geo_output)))
+def make_geojson():
+    users = open('users.txt', 'r')
+    output = open('output.geojson', 'w')
 
-users.close()
-output.close()
+    geo_output = []
+
+    for line in users:
+        elements = line.split('"')
+
+        coords = elements[0].strip(' ')
+        coords = coords.split(',')
+        latitude = float(coords[0])
+        longitude = float(coords[1])
+        name = elements[1].strip()
+        comment = elements[2].strip()
+        comment = comment[2:]
+
+        point = Point((longitude, latitude))
+        feature = Feature(geometry=point, properties={"Name": name})
+
+        geo_output.append(feature)
+
+    output.write(dumps(FeatureCollection(geo_output)))
+
+    users.close()
+    output.close()
