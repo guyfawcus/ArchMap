2da783fb62df4ddd016f2e9d14372f895a8adcaa
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-04-19 18:54:04 +0100

    Remove 'Johannes Löthberg' from copyright info
    Only temporary, the readthedocs build was failing because of the
    non-ASCII 'ö'. Needs fixing.

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ArchMap'
-copyright = '2014, James Fawcus-Robinson, Johannes Löthberg'
+copyright = '2014, James Fawcus-Robinson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
