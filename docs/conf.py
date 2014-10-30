e4cb2cf3212453d3526ff6e0f5c624a8ba615161
James Fawcus-Robinson <jameslmfr@gmail.com>
2014-10-30 01:54:31 +0000

    Use 'napoleon' from 'sphinxcontrib'
    For use with Sphinx under v1.3

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 # Version 1.3 is required for Napoleon.
-needs_sphinx = '1.3'
+#needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
